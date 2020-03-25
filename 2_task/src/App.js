5b530fc4782f1dad0b0fea9d35879779a0931a23
KataV <KataV@users.noreply.github.com>
2020-03-25 14:30:58 +0200

    Changed props to state and it works!

--- 2_task/src/App.js
+++ 2_task/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
         {name: 'Bob', species: 'thing'},
         {name: 'Janet', species: 'succubus'},
         {name: 'Ted', species: 'dudebro'},
-        {name: 'Blob', species: 'blob'}
+        {name: 'Blib', species: 'blob'}
     ]
 
   };
@@ -24,11 +24,11 @@ class App extends Component {
       <div>
       <h1>Hello World!</h1>
       <p>Hello world!</p>
-      <Person name="Kata" species="reptilian" />
-      <Person name="Bob" species="thing"/> 
-      <Person name="Janet" species="succubus"/> 
-      <Person name="Ted" species="dudebro"/> 
-      <Person name="Blob" species="blob" />
+      <Person name={this.state.persons[0].name} species={this.state.persons[0].species} />
+      <Person name={this.state.persons[1].name} species={this.state.persons[1].species}/> 
+      <Person name={this.state.persons[2].name} species={this.state.persons[2].species}/> 
+      <Person name={this.state.persons[3].name} species={this.state.persons[3].species}/> 
+      <Person name={this.state.persons[4].name} species={this.state.persons[4].species} />
       </div>
       );
   }
