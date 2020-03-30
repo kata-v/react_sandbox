c8b180325a2e3c916015077e1ce20f454a58a389
KataV <KataV@users.noreply.github.com>
2020-03-30 11:13:35 +0300

    Added three buttons and their functionality to add, remove and to reset likes.

--- 3_task/src/App.js
+++ 3_task/src/App.js
@@ -3,16 +3,34 @@ import './App.css';
 
 class App extends Component {
   state = {
-    likes: 666
+    likes: 0
 
   };
+
+  addLikesHandler= () => {
+console.log('test');
+this.setState({likes: this.state.likes +1})
+  };
+
+  removeLikeHandler= () => {
+    console.log('test');
+    this.setState({likes: this.state.likes -1})
+      };
+
+  resetLikesHandler= () => {
+        console.log('test');
+        this.setState({likes: this.state.likes = 0})
+          };
+
   render(){
     return (
       <div>
         <h1>
           Total likes: {this.state.likes}
         </h1>
-
+<button onClick={this.addLikesHandler}>Add like</button>
+<button onClick={this.removeLikeHandler}>Unlike</button>
+<button onClick={this.resetLikesHandler}>Reset</button>
       </div>
     );
   }
