a55229023aab0e17b9d0fac0109ee71fed6fb0d3
KataV <KataV@users.noreply.github.com>
2020-04-06 13:33:44 +0300

    Added circles to click and styling

--- 5_speed_game/src/App.js
+++ 5_speed_game/src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Circle from './Circle/Circle'
 
-function App() {
+class App extends Component{
+  state = {
+ score: 0,
+  };
+
+  clickHandler = (circleID) => {
+    console.log("I was clicked!", circleID);
+    this.setState({
+      score: this.state.score +1
+    })
+  };
+
+  render(){
   return (
     <div className="App">
-      
+      <h1>---Game of Speed---</h1>
+      <p>Your score is: {this.state.score}</p>
+      <main>
+        <Circle clickyclick={this.clickHandler.bind(this, "number one")}/>
+        <Circle clickyclick={this.clickHandler.bind(this, 2)}/>
+        <Circle clickyclick={this.clickHandler.bind(this, 3)}/>
+        <Circle clickyclick={this.clickHandler.bind(this, 4)}/>
+      </main>
+      <button>Start Game</button>
+      <button>Give up</button>
     </div>
   );
+}
 }
 
 export default App;
---
+++ 5_speed_game/src/Circle/Circle.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import "./Circle.css";
+
+const Circle = props => {
+   return  <div className='circle'onClick={props.clickyclick}></div>;
+};
+
+export default Circle;
